c8d3d9d157f530a93ba45e7d32f0a194cbe1629e
Vinícius Melo <vinicius.apol.melo@gmail.com>
2022-06-06 11:42:06 -0300

    barra de status

--- src/components/StatsScreen.tsx
+++ src/components/StatsScreen.tsx
@@ -6,18 +6,22 @@ import { AppContext } from '../data/api/Store'
 function StatsScreen() {
 
     const { stats, loading, index, evolutionChain } = useContext(AppContext)
-    // console.log(stats[index])
+    console.log(stats[index])
 
 
     return (
         <>
             {!loading && <div className={styles.Main_Screen}>
                 <p>
-                    {evolutionChain[index].chain.evolves_to[0].species.name}
+                    {stats[index].name}
+                    {/* {evolutionChain[index].chain.evolves_to[0].species.name} */}
                 </p>
                 <img
-                    src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${stats[index].id + 1}.png`}
+                    src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/${stats[index].id}.png`}
                     alt="Forma Evoluída do Pokemon" />
+                <ul>
+                    {stats[index].stats.map((stat: any, index: number) => <li key={index}>{stat.stat.name} {stat.base_stat}<div style={{ width: `${stat.base_stat <= 100 ? stat.base_stat : 100}%` }}></div></li>)}
+                </ul>
             </div>}
         </>
 
