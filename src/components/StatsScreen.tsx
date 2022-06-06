8b22e9737da6dc61aa9cce724325e0507adf6940
Vinícius Melo <vinicius.apol.melo@gmail.com>
2022-06-06 13:12:18 -0300

    evolução

--- src/components/StatsScreen.tsx
+++ src/components/StatsScreen.tsx
@@ -12,16 +12,33 @@ function StatsScreen() {
     return (
         <>
             {!loading && <div className={styles.Main_Screen}>
-                <p>
+                <h1>
                     {stats[index].name}
-                    {/* {evolutionChain[index].chain.evolves_to[0].species.name} */}
-                </p>
+
+                </h1>
                 <img
                     src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/${stats[index].id}.png`}
                     alt="Forma Evoluída do Pokemon" />
-                <ul>
-                    {stats[index].stats.map((stat: any, index: number) => <li key={index}>{stat.stat.name} {stat.base_stat}<div style={{ width: `${stat.base_stat <= 100 ? stat.base_stat : 100}%` }}></div></li>)}
-                </ul>
+                <div className={styles.lis}>
+                    <ul>
+                        {stats[index].stats.map((stat: any, index: number) =>
+                            <li key={index}>
+                                {stat.stat.name}
+                                <div style={{ width: `${stat.base_stat <= 100 ? stat.base_stat : 100}%` }}>
+                                    <p>{stat.base_stat}</p>
+                                </div>
+                            </li>
+                        )}
+                    </ul>
+                    <ul style={{ alignSelf: 'center' }}>
+                        {stats[index].abilities.map((ab: any, index: number) =>
+                            <li key={index}>
+                                {`Habilidade ${index + 1}:`} {ab.ability.name}
+                            </li>
+                        )}
+                        <li style={{ marginTop: '30px' }}>Evolui para: {evolutionChain[index].chain.evolves_to[0].species.name}</li>
+                    </ul>
+                </div>
             </div>}
         </>
 
