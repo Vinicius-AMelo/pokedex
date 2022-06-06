ca822d7bede027b78fdc21e037e4c40886975b70
Vinícius Melo <vinicius.apol.melo@gmail.com>
2022-06-06 16:54:50 -0300

    pokedex

--- src/components/StatsScreen.tsx
+++ src/components/StatsScreen.tsx
@@ -6,43 +6,58 @@ import { AppContext } from '../data/api/Store'
 function StatsScreen() {
 
     const { stats, loading, index, evolutionChain } = useContext(AppContext)
-    console.log(evolutionChain[index])
+    console.log(stats[index])
 
 
 
 
     return (
         <>
-            {!loading && <div className={styles.Main_Screen}>
-                <h1>
-                    {stats[index].name}
-                </h1>
-                <img
-                    src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/${stats[index].id}.png`}
-                    alt="Forma Evoluída do Pokemon" />
-                <div className={styles.lis}>
-                    <ul>
-                        {stats[index].stats.map((stat: any, index: number) =>
-                            <li key={index}>
-                                {stat.stat.name}
-                                <div style={{ width: `${stat.base_stat <= 100 ? stat.base_stat : 100}%` }}>
-                                    <p>{stat.base_stat}</p>
+            {!loading &&
+                <div className={styles.Main_Screen}>
+                    <div>
+                        <div className={styles.Screen}>
+                            <div className={styles.Left_Screen}>
+                                <h1>
+                                    {stats[index].name}
+                                </h1>
+                                <ul>
+                                    {stats[index].stats.map((stat: any, index: number) =>
+                                        <li key={index}>
+                                            {stat.stat.name}
+                                            <div style={{ width: `${stat.base_stat <= 100 ? stat.base_stat : 100}%` }}>
+                                                <p>{stat.base_stat}</p>
+                                            </div>
+                                        </li>
+                                    )}
+                                </ul>
+                            </div>
+                            <div className={styles.Right_Screen}>
+                                <div className={styles.Pokemon_BG}>
+                                    <img
+                                        src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/${stats[index].id}.png`}
+                                        alt="Forma Evoluída do Pokemon" />
+                                    <div className={styles.Types}>
+                                        {stats[index].types.map((type: any, index: number) => <span key={index}>{type.type.name}</span>)}
+                                    </div>
+                                    <ul style={{ alignSelf: 'center' }}>
+                                        {stats[index].abilities.map((ab: any, index: number) =>
+                                            <li key={index}>
+                                                {`Habilidade ${index + 1}:`} {ab.ability.name}
+                                            </li>
+                                        )}
+                                    </ul>
                                 </div>
-                            </li>
-                        )}
-                    </ul>
-                    <ul style={{ alignSelf: 'center' }}>
-                        {stats[index].abilities.map((ab: any, index: number) =>
-                            <li key={index}>
-                                {`Habilidade ${index + 1}:`} {ab.ability.name}
-                            </li>
-                        )}
-                        <li style={{ marginTop: '30px' }}><p>Evolui para: {evolutionChain[index].chain.evolves_to[0].species.name === stats[index].name ? evolutionChain[index].chain.evolves_to[0].evolves_to[0].species.name : evolutionChain[index].chain.evolves_to[0].species.name}</p>
-                            <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/${stats[index].id + 1}.png`} alt="" />
-                        </li>
-                    </ul>
-                </div>
-            </div>}
+                                <ul>
+                                    <li style={{ marginTop: '30px' }}><p>Evolui para: {evolutionChain[index].chain.evolves_to[0].species.name === stats[index].name ? evolutionChain[index].chain.evolves_to[0].evolves_to[0].species.name : evolutionChain[index].chain.evolves_to[0].species.name}</p>
+                                        <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/${stats[index].id + 1}.png`} alt="" />
+                                    </li>
+                                </ul>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>}
         </>
 
     )
